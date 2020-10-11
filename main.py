5f255034a8a8a10551c6880931f325d9f62007ae
Kipras Januska <kiprasjanuska0@gmail.com>
2020-10-11 13:57:22 -0400

    added goat search

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# source
+# https://github.com/kxvxnc/Stockx-Discord-Bot/blob/master/main.py
+
 import discord
 from discord.ext import commands
 import json
@@ -10,7 +13,7 @@ selected = 0
 numResults = 0
 
 
-async def lookup(selection, keywords, ctx):
+async def lookup_stockx(selection, keywords, ctx):
     json_string = json.dumps({"params": f"query={keywords}&hitsPerPage=20&facets=*"})
     byte_payload = bytes(json_string, "utf-8")
     algolia = {
@@ -68,13 +71,77 @@ async def lookup(selection, keywords, ctx):
     await ctx.send(embed=embed)
 
 
+def lookup_goat(selection, keywords, ctx):
+    json_string = json.dumps({"params": f"query={keywords}&hitsPerPage=20&facets=*"})
+    byte_payload = bytes(json_string, "utf-8")
+    algolia = {
+        "x-algolia-agent": "Algolia for vanilla JavaScript 3.25.1",
+        "x-algolia-application-id": "2FWOTDVM2O",
+        "x-algolia-api-key": "ac96de6fef0e02bb95d433d8d5c7038a",
+    }
+    header = {
+        "accept": "*/*",
+        "accept-encoding": "gzip, deflate, br",
+        "accept-language": "en-US,en;q=0.9,lt;q=0.8",
+        "appos": "web",
+        "appversion": "0.1",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36",
+    }
+    with requests.Session() as session:
+        r = session.post(
+            "https://2fwotdvm2o-dsn.algolia.net/1/indexes/product_variants_v2/query",
+            params=algolia,
+            verify=False,
+            data=byte_payload,
+            timeout=30,
+        )
+        results = r.json()["hits"][0]
+        apiurl_prices = f"https://www.goat.com/web-api/v1/product_variants?productTemplateId={results['slug']}"
+        apiurl_general = (
+            f"https://www.goat.com/web-api/v1/product_templates/{results['slug']}"
+        )
+
+    response_prices = requests.get(apiurl_prices, verify=False, headers=header)
+    response_general = requests.get(apiurl_general, verify=False, headers=header)
+
+    prices = response_prices.json()
+    general = response_general.json()
+    sizes = []
+    for size in prices:
+        if (
+            size["boxCondition"] == "good_condition"
+            and size["shoeCondition"] == "new_no_defects"
+        ):
+            sizes.append(size)
+
+    embed = discord.Embed(
+        title=f"{general['name']}",
+        url=f"https://www.goat.com/sneakers/{general['slug']}'",
+        color=0x099F5F,
+    )
+    embed.set_thumbnail(url=general["gridPictureUrl"])
+    if "sku" in general:
+        embed.add_field(name="SKU:", value=general["sku"], inline=True)
+    else:
+        embed.add_field(name="SKU:", value="N/A", inline=True)
+    embed.add_field(name="‎⠀", value="⠀", inline=False)
+    for size in sizes:
+        lowestPrice = int(size["lowestPriceCents"]["amountUsdCents"] / 100)
+        embed.add_field(
+            name=size["size"],
+            value=f"Lowest Ask: ${lowestPrice}",
+            inline=True,
+        )
+    await ctx.send(embed=embed)
+
+
 @client.command(pass_context=True)
 async def logout(ctx):
     await client.logout()
 
 
 @client.command(pass_context=True)
-async def pc(ctx, *args):
+async def pcs(ctx, *args):
     keywords = ""
     for word in args:
         keywords += word + "%20"
@@ -96,7 +163,35 @@ async def pc(ctx, *args):
         numResults = len(r.json()["hits"])
 
     if numResults != 0:
-        await lookup(0, keywords, ctx)
+        await lookup_stockx(0, keywords, ctx)
+    else:
+        await ctx.send("No products found. Please try again.")
+
+
+@client.command(pass_context=True)
+async def pcg(ctx, *args):
+    keywords = ""
+    for word in args:
+        keywords += word + "%20"
+    json_string = json.dumps({"params": f"query={keywords}&hitsPerPage=20&facets=*"})
+    byte_payload = bytes(json_string, "utf-8")
+    algolia = {
+        "x-algolia-agent": "Algolia for vanilla JavaScript 3.25.1",
+        "x-algolia-application-id": "2FWOTDVM2O",
+        "x-algolia-api-key": "ac96de6fef0e02bb95d433d8d5c7038a",
+    }
+    with requests.Session() as session:
+        r = session.post(
+            "https://2fwotdvm2o-dsn.algolia.net/1/indexes/product_variants_v2/query",
+            params=algolia,
+            verify=False,
+            data=byte_payload,
+            timeout=30,
+        )
+        numResults = len(r.json()["hits"])
+
+    if numResults != 0:
+        await lookup_goat(0, keywords, ctx)
     else:
         await ctx.send("No products found. Please try again.")
 
